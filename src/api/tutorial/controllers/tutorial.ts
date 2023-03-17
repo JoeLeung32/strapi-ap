cf1cf5f7ced66c6cf206f0e01e498f8aa0df57ab
Joe Leung <joeleung32@yahoo.com.hk>
2023-03-18 02:48:20 +0800

    Populating createdBy and updatedBy. https://docs.strapi.io/dev-docs/api/rest/populate-select#populating-createdby-and-updatedby

--- src/api/tutorial/controllers/tutorial.ts
+++ src/api/tutorial/controllers/tutorial.ts
@@ -1,7 +1,31 @@
-/**
- * tutorial controller
- */
+'use strict';
 
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::tutorial.tutorial');
+const {createCoreController} = require('@strapi/strapi').factories;
+module.exports = createCoreController('api::tutorial.tutorial', ({strapi}) => ({
+    async find(ctx) {
+        // Calling the default core action
+        const {data, meta} = await super.find(ctx);
+        const query = strapi.db.query('api::tutorial.tutorial');
+        await Promise.all(
+            data.map(async (item, index) => {
+                const foundItem = await query.findOne({
+                    where: {
+                        id: item.id,
+                    },
+                    populate: ['createdBy', 'updatedBy'],
+                });
+                data[index].attributes.createdBy = {
+                    id: foundItem.createdBy.id,
+                    firstname: foundItem.createdBy.firstname,
+                    lastname: foundItem.createdBy.lastname,
+                };
+                data[index].attributes.updatedBy = {
+                    id: foundItem.updatedBy.id,
+                    firstname: foundItem.updatedBy.firstname,
+                    lastname: foundItem.updatedBy.lastname,
+                };
+            })
+        );
+        return {data, meta};
+    },
+}));
